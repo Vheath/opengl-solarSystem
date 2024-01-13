aa07e5829be07ef209179351b82209ccc8d0f9ab
Vheath <Timurkruz@mail.ru>
2024-01-13 12:17:20 +0300

    Base project
    Bunch of cubes, dir lighting and movement

---
+++ include/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+#include <glm/vec3.hpp>
+#include <glad/glad.h>
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+inline glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+#endif  // !COMMON_H
---
+++ include/shader.h
@@ -0,0 +1,61 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <fstream>
+#include <glm/ext/vector_float3.hpp>
+#include <glm/mat4x4.hpp>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+class Shader {
+   public:
+    // the program ID
+    unsigned int ID;
+
+    // constructor reads and builds the Shader
+    Shader(const char* vertexPath, const char* fragmentPath);
+    // use/active the shader
+    void use();
+    // utility uniform functions
+    void setBool(const std::string& name, bool value) const;
+    void setInt(const std::string& name, int value) const;
+    void setFloat(const std::string& name, float value) const;
+    void setVec3(const std::string& name, float x, float y, float z) const;
+    void setVec3(const std::string& name, const glm::vec3 vec) const;
+    void setMat4(const std::string& name, const glm::mat4& mat) const;
+
+   private:
+    void checkCompileErrors(unsigned int shader, std::string type) {
+        int success;
+        char infoLog[1024];
+        if (type != "PROGRAM") {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                std::cout
+                    << "ERROR::SHADER_COMPILATION_ERROR of type: " << type
+                    << "\n"
+                    << infoLog
+                    << "\n -- "
+                       "--------------------------------------------------- -- "
+                    << std::endl;
+            }
+        } else {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success) {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cout
+                    << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n"
+                    << infoLog
+                    << "\n -- "
+                       "--------------------------------------------------- -- "
+                    << std::endl;
+            }
+        }
+    }
+};
+#endif  // !SHADER_H
+
